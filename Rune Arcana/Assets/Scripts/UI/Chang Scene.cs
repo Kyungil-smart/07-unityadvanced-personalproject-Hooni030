fbea726b183fdf83c475da13a2ca2d7a47f09362
Hooni0_0 <startrack0515@gmail.com>
2026-02-27 12:50:07 +0900

    [Update] 타이틀 씬 화면전환
    타이틀 씬 화면 전환 연출

--- Rune Arcana/Assets/Scripts/UI/Chang Scene.cs
+++ Rune Arcana/Assets/Scripts/UI/Chang Scene.cs
@@ -5,34 +5,56 @@ using UnityEngine;
 
 public class ChangScene : MonoBehaviour
 {
-    private static readonly int Out = Animator.StringToHash("Change");
+    private static readonly int Out = Animator.StringToHash("Out");
     
     [SerializeField] private Animator _animator;
-    [SerializeField] [Range(0.1f, 10.0f)] private float _waitTime = 1f;
+    [SerializeField] private Canvas  _canvas;
+    
+    [SerializeField] [Range(0.1f, 10.0f)] private float _inTime = 1f;
+    [SerializeField] [Range(0.1f, 10.0f)] private float _outTime = 2f;
+    
     private AudioSource _source;
     
-
     private void Awake()
     {
+        _canvas = GetComponent<Canvas>();
         _animator = GetComponent<Animator>();
         _source = GetComponent<AudioSource>();
     }
 
-    public void ChangeScene()
+    private void Start()
     {
-        StartCoroutine(Change(_waitTime));
-        _animator.SetBool(Out, true);
+        TurnOnScene();
+    }
+
+    private void TurnOnScene()
+    {
+        StartCoroutine(OnScene(_inTime));
+    }
+
+    public void TurnOffScene()
+    {
+        _canvas.sortingOrder = 10;
+        StartCoroutine(OffScene(_outTime));
     }
     
     private void OnDisable()
     {
-        StopCorouito
+        StopAllCoroutines();
     }
-    
 
-    private IEnumerator Change(float time)
+    private IEnumerator OffScene(float time)
     {
-        yield return YieldContainer.Wait(time);
+        _canvas.sortingOrder = 10;
+        _animator.SetBool(Out, true);
+        yield return new WaitForSeconds(time);
         GameSceneManager.Instance.ChangeScene((int)SceneIndex.Tutorial);
+    }
+
+    private IEnumerator OnScene(float time)
+    {
+        _animator.SetBool(Out, false);
+        yield return YieldContainer.Wait(time);
+        _canvas.sortingOrder = -1;
     }
 }
